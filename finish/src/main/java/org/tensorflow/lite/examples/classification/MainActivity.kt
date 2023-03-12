65c23e8c23db664cc585285920a9f114249ca296
thierry <thierryghaddad@gmail.com>
2023-03-12 16:18:36 -0700

    add keep screen on feature

--- finish/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.kt
+++ finish/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.kt
@@ -25,6 +25,7 @@ import android.graphics.Matrix
 import android.os.Bundle
 import android.util.Log
 import android.util.Size
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -85,6 +86,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         // Request camera permissions
         if (allPermissionsGranted()) {
